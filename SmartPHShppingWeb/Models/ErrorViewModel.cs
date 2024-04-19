e0160d37d0f7b2a3e68229277c107db6597b4e7a
Sudip Kumar Mandal <mandal7031@gmail.com>
2024-04-19 21:58:18 +0530

    Add project files.

---
+++ SmartPHShppingWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartPHShppingWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
